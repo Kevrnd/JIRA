a410da29ac330a9423d182b84eafea5c16b477df
Evgeny <48059713+Kevrnd@users.noreply.github.com>
2023-06-22 00:04:19 +0300

    Update validatorTMHRv2CheckDaysHoursRemains.groovy
    Test 22/06/2023 Изменение валидатора для группы Support, а так же добавлено уведомление пользователю "Проверьте правильность заполнения поля 'Time of absence in hours'! Вы можете указать... В указанных вами днях отпуска ${daysOffInLeaveAndHolidays} - являются праздником/выходным,  поэтому они не учитываются

--- com / projects / TMHRV2 /validatorTMHRv2CheckDaysHoursRemains.groovy
+++ com / projects / TMHRV2 /validatorTMHRv2CheckDaysHoursRemains.groovy
@@ -1,4 +1,5 @@
 package com.slotegrator.projects.TMHRV2
+import java.lang.String
 import java.lang.Double
 import com.atlassian.jira.issue.CustomFieldManager
 import com.atlassian.jira.jql.parser.JqlQueryParser
@@ -16,7 +17,7 @@ import com.slotegrator.projects.TMHRV2.HolidayThisYear
 
 if (! (issue.issueType.name == 'Vacation' || issue.issueType.name == 'Sick day') ) {
     return 
-}
+} 
 List<Timestamp> holidays = new HolidayThisYear().holidays
 def daysToDecrease = 0
 BigDecimal hoursInWorkDay = 0 
@@ -28,12 +29,13 @@ String userGroupCheck = ComponentAccessor.groupManager.isUserInGroup(currentUser
 
 Timestamp dueDate_date_timestamp = issue.getDueDate()
 CustomFieldManager cfm = ComponentAccessor.getCustomFieldManager()
-Object customFieldStartDate = cfm.getCustomFieldObject(11016L)
-Object remainingVacationDays = cfm.getCustomFieldObject(13302L)  
-Object remainingFamilyDays = cfm.getCustomFieldObject(13304L)  
+Object customFieldStartDate = cfm.getCustomFieldObject(11016L)  // change
+Object remainingVacationDays = cfm.getCustomFieldObject(13301L)   // change
+Object remainingFamilyDays = cfm.getCustomFieldObject(13303L)     //change
 Object customFieldTimeofAbsence = cfm.getCustomFieldObject(11100L)
-Object remainingSickDay = cfm.getCustomFieldObject(13306L) 
-Calendar calendar = Calendar.getInstance()
+
+Object remainingSickDay = cfm.getCustomFieldObject(13305L) 
+Calendar calendar = Calendar.getInstance() 
 Timestamp start_date_timestamp = issue.getCustomFieldValue(customFieldStartDate)
 def duedate_minus_start_date = issue.getDueDate() - start_date_timestamp
 
@@ -46,18 +48,191 @@ Double remainingFamilyDaysValue = remainingFamilyDays.getValue(cardIssue)
 Double remainingSickDayValue = remainingSickDay.getValue(cardIssue)
 calendar.setTime(start_date_timestamp)
 int dayOfWeeks = calendar.get(Calendar.DAY_OF_WEEK)
-BigDecimal timeOfAbsence = issue.getCustomFieldValue(customFieldTimeofAbsence) as BigDecimal
-if (userGroupCheck == "true"){
-    hoursInWorkDay = 7.5
-} else {
-    hoursInWorkDay = 8
+BigDecimal timeOfAbsence = issue.getCustomFieldValue(customFieldTimeofAbsence) as BigDecimal;// change double
+
+def daysOffInLeaveAndHolidays = ""//!!!!!!!!!!!!!!!!!!!\\\\\\\\\\!!!!!!!!!! Don't forget
+String hoursSuffix(int hours) {
+    if (hours % 10 == 1 && hours % 100 != 11) {
+        return ""
+    } else if ([2, 3, 4].contains(hours % 10) && ![12, 13, 14].contains(hours % 100)) {
+        return "а"
+    } else {
+        return "ов"
+    }
 }
-if (issue.issueType.name == 'Vacation'){     
+
+switch (issue.issueType.name){
+    case 'Vacation' :
+    // Для пользователей из группы Support
+    if (userGroupCheck == "true"){
+        hoursInWorkDay = 7.5
+        //Проверка отпуска на один день
+        if (duedate_minus_start_date == 0) {
+            //Количество часов соотвествует  одному рабочему дню или половине исходя из группы пользователя
+            if (timeOfAbsence / hoursInWorkDay != 0.5 && timeOfAbsence / hoursInWorkDay != 1) {
+                throw new InvalidInputException("Вы можете взять только половину дня или целый день отпуска.")
+            }
+            // Проверяем, превышает ли запрошенное количество часов отпуска имеющийся остаток у пользователя
+            if (remainingVacationDaysValue + remainingFamilyDaysValue < timeOfAbsence) {
+                throw new InvalidInputException("Количество часов отпуска в задаче превышает имеющийся у вас остаток.")
+            }
+        } else if (duedate_minus_start_date > 0) {
+            for (int i = 0; i <= duedate_minus_start_date; i++) {
+                daysToDecrease++
+            }
+        }
+        if (daysToDecrease >0) {
+        // Проверка имеющегося остатка дней у пользователя
+            if (remainingVacationDaysValue + remainingFamilyDaysValue < daysToDecrease * hoursInWorkDay - (hoursInWorkDay * 0.5)) {
+                throw new InvalidInputException("Количество дней отпуска в задаче превышает имеющийся у вас остаток.")
+            }
+            // Проверка заполнения поля при условии, что пользователь может взять X дней + 0.5 дня
+            if (!(timeOfAbsence == daysToDecrease * hoursInWorkDay || timeOfAbsence == daysToDecrease * hoursInWorkDay - hoursInWorkDay * 0.5)) {
+                String suffix = hoursSuffix((daysToDecrease * hoursInWorkDay- hoursInWorkDay * 0.5) as int)
+                throw new InvalidInputException("Проверьте правильность заполнения поля 'Time of absence in hours'! \
+                    Вы можете указать ${daysToDecrease * hoursInWorkDay}\
+                    или ${daysToDecrease * hoursInWorkDay - hoursInWorkDay * 0.5} час${suffix}.")
+
+            }
+        }
+    // Для пользователей не входящих в группу Support
+    } else {
+        hoursInWorkDay = 8
+        //Проверка отпуска на один день
+        if (duedate_minus_start_date == 0) {
+            if (dayOfWeeks == 1 || dayOfWeeks == 7 || start_date_timestamp in holidays) {
+                throw new InvalidInputException("Вы хотите взять отпуск в выходной или праздничный день.")
+            }
+
+            if (timeOfAbsence / hoursInWorkDay != 0.5 && timeOfAbsence / hoursInWorkDay != 1) {
+                throw new InvalidInputException("Вы можете взять только половину дня или целый день отпуска.")
+            }
+            // Проверяем, превышает ли запрошенное количество часов отпуска имеющийся остаток у пользователя
+            if (remainingVacationDaysValue + remainingFamilyDaysValue < timeOfAbsence) {
+                    throw new InvalidInputException("Количество часов отпуска в задаче превышает имеющийся у вас остаток.")
+            }
+
+        //Если отпуск не на один день
+        } else if (duedate_minus_start_date > 0) {
+            for (int i = 0; i <= duedate_minus_start_date; i++) {
+                calendar.setTime(start_date_timestamp + i)
+                int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK)
+                if (dayOfWeek == 1 || dayOfWeek == 7 || start_date_timestamp + i in holidays) {
+                    daysOffInLeaveAndHolidays += (start_date_timestamp + i).format("dd/MMM/yyyy") as String + " , "
+                    continue
+                }
+                daysToDecrease++
+            }
+        }
+
+        if (daysToDecrease >= 0) {
+        // Проверка имеющегося остатка дней у пользователя с учетом календаря (Выходные и праздники не учитываются)
+            if (remainingVacationDaysValue + remainingFamilyDaysValue < daysToDecrease * hoursInWorkDay - (hoursInWorkDay * 0.5)) {
+                throw new InvalidInputException("Количество дней отпуска в задаче превышает имеющийся у вас остаток.")
+            }
+        /*Данное сообщение пользователь увидит при условии указания всех дней отпуска в выходные или праздники 
+        Актуально для тех ситуаций, когда  2 праздника подряд, и пользователь хочет взять на них отпуск
+        Проверка заполнения поля при условии, что пользователь может взять X дней или X дней -0.5 дня
+        */
+            if (duedate_minus_start_date != 0 && daysToDecrease == 0 && !(timeOfAbsence == daysToDecrease * hoursInWorkDay || timeOfAbsence == daysToDecrease * hoursInWorkDay - hoursInWorkDay * 0.5)) {
+                String suffix = hoursSuffix((daysToDecrease * hoursInWorkDay) as int)
+                throw new InvalidInputException("Проверьте правильность заполнения поля 'Time of absence in hours'! \
+                    Вы можете указать ${daysToDecrease * hoursInWorkDay} час${suffix}.\
+                    В указанных вами днях отпуска ${daysOffInLeaveAndHolidays} - являются праздником/выходным, \
+                    поэтому они не учитываются")
+            }
+            if (!(timeOfAbsence == daysToDecrease * hoursInWorkDay || timeOfAbsence == daysToDecrease * hoursInWorkDay - hoursInWorkDay * 0.5)) {
+                String suffix = hoursSuffix((daysToDecrease * hoursInWorkDay- hoursInWorkDay * 0.5) as int)
+                throw new InvalidInputException("Проверьте правильность заполнения поля 'Time of absence in hours'! \
+                    Вы можете указать ${daysToDecrease * hoursInWorkDay}\
+                    или ${daysToDecrease * hoursInWorkDay - hoursInWorkDay * 0.5} час${suffix}.\
+                    В указанных вами днях отпуска ${daysOffInLeaveAndHolidays} - являются праздником/выходным, \
+                    поэтому они не учитываются.")
+            }
+        }
+    }
+
+    break
+
+    case 'Sick day' :
+    // Для пользователей из группы Support
+    if (userGroupCheck == "true"){
+        hoursInWorkDay = 7.5 
+        throw new InvalidInputException("STOP TRUE")
+
+    // Для пользователей не входящих в группу Support
+    } else {
+        hoursInWorkDay = 8
+        //Проверка SickDay на один день
+        if (duedate_minus_start_date == 0) {
+            if (dayOfWeeks == 1 || dayOfWeeks == 7 || start_date_timestamp in holidays) {
+                throw new InvalidInputException("Вы хотите взять SickDay в выходной или праздничный день.")
+            }
+
+            if (timeOfAbsence / hoursInWorkDay != 0.5 && timeOfAbsence / hoursInWorkDay != 1) {
+                throw new InvalidInputException("Вы можете взять только половину дня или целый день Sick day.")
+            }
+
+            if (remainingSickDayValue < timeOfAbsence) {
+                throw new InvalidInputException("Количество часов SickDay в задаче превышает имеющийся у вас остаток.")
+            }
+        //Если отпуск не на один день
+        } else if (duedate_minus_start_date > 0) {
+            for (int i = 0; i <= duedate_minus_start_date; i++) {
+                calendar.setTime(start_date_timestamp + i)
+                int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK)
+                if (dayOfWeek == 1 || dayOfWeek == 7 || start_date_timestamp + i in holidays) {
+                    daysOffInLeaveAndHolidays += (start_date_timestamp + i).format("dd/MMM/yyyy") as String + " , "
+                    continue
+                }
+                daysToDecrease++
+            }
+        }
+
+        if (daysToDecrease >= 0) {
+        // Проверка имеющегося остатка дней у пользователя с учетом календаря (Выходные и праздники не учитываются)
+            if (remainingSickDayValue < daysToDecrease * hoursInWorkDay - (hoursInWorkDay * 0.5)) {
+                throw new InvalidInputException("Количество дней SickDay в задаче превышает имеющийся у вас остаток.")
+            }
+        /*Данное сообщение пользователь увидит при условии указания всех дней отпуска в выходные или праздники 
+        Актуально для тех ситуаций, когда  2 праздника подряд, и пользователь хочет взять на них отпуск
+        Проверка заполнения поля при условии, что пользователь может взять X дней или X дней -0.5 дня
+        */
+            if (duedate_minus_start_date != 0 && daysToDecrease == 0 && !(timeOfAbsence == daysToDecrease * hoursInWorkDay || timeOfAbsence == daysToDecrease * hoursInWorkDay - hoursInWorkDay * 0.5)) {
+                String suffix = hoursSuffix((daysToDecrease * hoursInWorkDay) as int)
+                throw new InvalidInputException("Проверьте правильность заполнения поля 'Time of absence in hours'! \
+                    Вы можете указать ${daysToDecrease * hoursInWorkDay} час${suffix}.\
+                    В указанных вами днях Sick days ${daysOffInLeaveAndHolidays} - являются праздником/выходным, \
+                    поэтому они не учитываются")
+            }
+            if (!(timeOfAbsence == daysToDecrease * hoursInWorkDay || timeOfAbsence == daysToDecrease * hoursInWorkDay - hoursInWorkDay * 0.5)) {
+                String suffix = hoursSuffix((daysToDecrease * hoursInWorkDay- hoursInWorkDay * 0.5) as int)
+                throw new InvalidInputException("Проверьте правильность заполнения поля 'Time of absence in hours'! \
+                    Вы можете указать ${daysToDecrease * hoursInWorkDay}\
+                    или ${daysToDecrease * hoursInWorkDay - hoursInWorkDay * 0.5} час${suffix}.\
+                    В указанных вами днях Sick day ${daysOffInLeaveAndHolidays} - являются праздником/выходным, \
+                    поэтому они не учитываются.")
+            }
+        }
+    }
+    break 
+
+}
+
+
+
+
+
+ 
+
+/* Old version
+
     if (duedate_minus_start_date == 0){
         if (!((dayOfWeeks == 1) || (dayOfWeeks == 7) || start_date_timestamp in holidays )) { // 1 - SUN; 7 SUT
             if ((remainingVacationDaysValue + remainingFamilyDaysValue)  < timeOfAbsence ){
-                throw new InvalidInputException("Количество часов отпуска в задаче превышает имеющейся у вас остаток.")
+                throw new InvalidInputException("Количество дней отпуска в задаче превышает имеющейся у вас остаток.")
             }
+            log.error("timeOfAbsence / hoursInWorkDay" + !((timeOfAbsence / hoursInWorkDay == 0.5) || (timeOfAbsence / hoursInWorkDay == 1)))
             if ( !((timeOfAbsence / hoursInWorkDay == 0.5) || (timeOfAbsence / hoursInWorkDay == 1))) {
                 throw new InvalidInputException("Вы можете взять только половину дня или целый день отпуска.")
             }
@@ -70,6 +245,9 @@ if (issue.issueType.name == 'Vacation'){
             calendar.setTime(start_date_timestamp + i)
             int dayOfWeek = calendar.get(Calendar.DAY_OF_WEEK)
             if ((dayOfWeek == 1) || (dayOfWeek == 7) || start_date_timestamp + i in holidays ) { // 1 - SUN; 7 SUT
+         
+                
+                daysOffInLeaveAndHolidays += (start_date_timestamp + i).format("dd/MMM/yyyy") as String + " , " 
                 continue
             }
             daysToDecrease++
@@ -77,25 +255,35 @@ if (issue.issueType.name == 'Vacation'){
     }
 
 
-    if (daysToDecrease > 0){
-        //Проверка имеющегося остатка дней у пользователя с учетом календаря (Выходные и праздники не учитываются)
+    if (daysToDecrease >= 0){
+     //Проверка имеющегося остатка дней у пользователя с учетом календаря (Выходные и праздники не учитываются)
         if ((remainingVacationDaysValue + remainingFamilyDaysValue) <  (daysToDecrease * hoursInWorkDay - (hoursInWorkDay * 0.5) )) {
             throw new InvalidInputException("Количество дней отпуска в задаче превышает имеющейся у вас остаток.")
         }
         // Проверка заполнения поля при условии, что пользователь может взять X дней + 0.5 дня
         if ( !(timeOfAbsence == daysToDecrease * hoursInWorkDay || timeOfAbsence == daysToDecrease * hoursInWorkDay - hoursInWorkDay * 0.5) ){
-            throw new InvalidInputException('Проверьте правильность заполнения поля "Time of absence in hours"!')
+            throw new InvalidInputException("Проверьте правильность заполнения поля 'Time of absence in hours'! \
+            Вы можете указать ${daysToDecrease* hoursInWorkDay}\
+            или ${daysToDecrease *hoursInWorkDay- hoursInWorkDay * 0.5} часов(а)\
+            В указанных вами днях отпуска ${daysOffInLeaveAndHolidays} - являются праздником/выходным, \
+            поэтому они не учитываются")
         }
     }
-}
-// This block was added 2023-06-12 issue JS-7204 
+} 
+
+
+
+
+
+
+
 if (issue.issueType.name == 'Sick day'){
     if (duedate_minus_start_date == 0){
         if (!((dayOfWeeks == 1) || (dayOfWeeks == 7) || start_date_timestamp in holidays )) { // 1 - SUN; 7 SUT
-            if (remainingSickDayValue < timeOfAbsence ){
-                throw new InvalidInputException("Количество часов Sick Days в задаче превышает имеющейся у вас остаток.")
+            if ((remainingSickDayValue)  < timeOfAbsence ){
+                throw new InvalidInputException("Количество дней Sick Days в задаче превышает имеющейся у вас остаток.")
             }
-            log.warn("Вы можете взять только половину дня или целый день Sick day.  " + !((timeOfAbsence / hoursInWorkDay == 0.5) || (timeOfAbsence / hoursInWorkDay == 1)))
+            log.error("timeOfAbsence / hoursInWorkDay" + !((timeOfAbsence / hoursInWorkDay == 0.5) || (timeOfAbsence / hoursInWorkDay == 1)))
             if ( !((timeOfAbsence / hoursInWorkDay == 0.5) || (timeOfAbsence / hoursInWorkDay == 1))) {
                 throw new InvalidInputException("Вы можете взять только половину дня или целый день Sick day.")
             }
@@ -116,7 +304,6 @@ if (issue.issueType.name == 'Sick day'){
     }
 
     if (daysToDecrease > 0){
-        log.warn ("daysToDecrease-->" + daysToDecrease + " hoursInWorkDay--> " + hoursInWorkDay)
         //Проверка имеющегося остатка дней у пользователя с учетом календаря (Выходные и праздники не учитываются)
         if ((remainingSickDayValue) <  (daysToDecrease * hoursInWorkDay - (hoursInWorkDay * 0.5) )) {
             throw new InvalidInputException("Количество дней Sick day в задаче превышает имеющейся у вас остаток.")
@@ -127,5 +314,5 @@ if (issue.issueType.name == 'Sick day'){
         }
     }
 }
-
+*/
 
