8a7c98974e825d7a465b2e26a61fb721827d95ca
Evgeny <48059713+Kevrnd@users.noreply.github.com>
2023-07-10 19:54:35 +0200

    10/07

--- createEpic in BF.groovy
+++ createEpic in BF.groovy
@@ -1,3 +1,6 @@
+/*   JS-7530	[Jira] Автоматизация создания Epic  */
+import com.atlassian.jira.issue.attachment.Attachment
+import com.atlassian.jira.issue.AttachmentManager
 import com.atlassian.jira.issue.ModifiedValue
 import com.atlassian.jira.issue.util.DefaultIssueChangeHolder
 import com.atlassian.jira.issue.util.IssueChangeHolder
@@ -17,76 +20,76 @@ import com.atlassian.jira.user.ApplicationUser
 
 IssueManager issueManager = ComponentAccessor.getIssueManager()
 ProjectManager projectManager = ComponentAccessor.getProjectManager()
-MutableIssue issue = issueManager.getIssueObject("BF-1448")
-
+//MutableIssue issue = issueManager.getIssueObject("BF-3200")
+log.warn ("Issue Type-->" + issue.issueType.name + "  IssueKey-->" + issue.key)
 // Check issue type
-if (issue.issueType.name != "Business Feature"){
-    log.warn (issue.issueType.name)
+if (issue.issueType.name != "Product request"){
+
     return
 }
-CustomFieldManager customFieldManager = ComponentAccessor.getCustomFieldManager()
 
-// Create epic 
-// Кого нужно ставить Assignee в Эпике?????????
-def createLinkedEpic(MutableIssue issue, String projectKey) {    
+
+// Create epic (IssueKey in which the epic is created, projectKey in which the epic is created)
+
+def createLinkedEpic(MutableIssue issue, String projectKey) {
     IssueManager issueManager = ComponentAccessor.getIssueManager()
+    ProjectManager projectManager = ComponentAccessor.projectManager
+    AttachmentManager attachmentManager = ComponentAccessor.getAttachmentManager()
     CustomFieldManager customFieldManager = ComponentAccessor.customFieldManager
     IssueService issueService = ComponentAccessor.issueService
-    Object authContext = ComponentAccessor.getJiraAuthenticationContext()
-    ApplicationUser currentUser = authContext.getLoggedInUser() //От какого пользователя создаем Эпик?
-    ProjectManager projectManager = ComponentAccessor.projectManager
-    Project project = projectManager.getProjectObjByKey(projectKey)    
-    String projectName = project ? project.getName() : null   
-
+    ApplicationUser automationUser = ComponentAccessor.userManager.getUserByName('automation')
+    ComponentAccessor.getJiraAuthenticationContext().setLoggedInUser(automationUser)
+    Project project = projectManager.getProjectObjByKey(projectKey)
+    String projectName = project ? project.getName() : null
     CustomField parentLinkField = customFieldManager.getCustomFieldObject(10112L)
-
-    String assigneUser = issue.assignee ? issue.assignee.getName() : "automation"
-    //String projectLead = project ? project.getProjectLead() : null
-    String reporterUser = issue.reporter.getName()
+    String projectLead = project ? project.getProjectLead().getName() : automationUser
     Object originalSummary = issue.getSummary()
     String epicNameFieldValue = issue.key + originalSummary
-    String epicSummary = "[" + projectName + "] " + originalSummary    
+    String epicSummary = "[" + projectName + "] " + originalSummary
     String originalDescription = issue.getDescription()
     String epicLink = issue.getCustomFieldValue(customFieldManager.getCustomFieldObject(10101L))
-    //Input parametrs Epic
+    //Set parametrs Epic
     def issueInputParameters = issueService.newIssueInputParameters().with {
-    setProjectId(project.id)
-    setIssueTypeId('10000')
-    setAssigneeId(assigneUser)
-    setReporterId(reporterUser)
-    setSummary(epicSummary)
-    setDescription(originalDescription)
-    setPriorityId(priorityId)
-    addCustomFieldValue(10103L, epicNameFieldValue)
+        setProjectId(project.id)
+        setAssigneeId(projectLead)
+        setIssueTypeId('10000')
+        setReporterId(automationUser.getName())
+        setSummary(epicSummary)
+        setDescription(originalDescription)
+        setPriorityId(priorityId)
+        addCustomFieldValue(10103L, epicNameFieldValue)
     }
     //Create Epic
-    Object validationResult = issueService.validateCreate(currentUser, issueInputParameters)
+    Object validationResult = issueService.validateCreate(automationUser, issueInputParameters)
     assert validationResult.valid : validationResult.errorCollection
-    def result = issueService.create(currentUser, validationResult)
+    def result = issueService.create(automationUser, validationResult)
     assert result.valid : result.errorCollection
+    //Link
     String newEpicKey = result.getIssue().getKey()
     MutableIssue newEpic = issueManager.getIssueObject(newEpicKey)
     def parentLinkFieldType = parentLinkField.getCustomFieldType()
-    JiraAuthenticationContext jiraAuthenticationContext = ComponentAccessor.getJiraAuthenticationContext();
     Object mutableParentIssue = parentLinkFieldType.getSingularObjectFromString(issue.key)
     Issue mutableIssue = issueManager.getIssueObject(newEpic.id)
     IssueChangeHolder changeHolder = new DefaultIssueChangeHolder()
     parentLinkField.updateValue(null, mutableIssue, new ModifiedValue(parentLinkField, mutableParentIssue), changeHolder)
+    //Attachments copy
+    Collection<Attachment> issueObject = issue.getAttachments()
+    attachmentManager.copyAttachments(issue, automationUser, newEpicKey)
     log.warn ( "New epic create: "+ newEpicKey + epicSummary)
-
 }
 
-CustomField  customFieldP2ProjectPicker= customFieldManager.getCustomFieldObject(13307L)
-def p2ProjectPickerValue =customFieldP2ProjectPicker.getValue(issue)
 
+CustomFieldManager customFieldManager = ComponentAccessor.getCustomFieldManager()
+CustomField  customFieldP2ProjectPicker= customFieldManager.getCustomFieldObject(13307L)
+def p2ProjectPickerValue = customFieldP2ProjectPicker.getValue(issue)
 
 if (p2ProjectPickerValue == null) {
     log.warn ("Поле P2 Projects не заполненно для ${issue.key}")
     return
-} else if (p2ProjectPickerValue.first() == "All P2 Projects"){ // Поле изменим для выбоа только одного значения
+} else if (p2ProjectPickerValue.first() == "All P2 Projects"){
     List<Project> projects = projectManager.getProjectObjects()
     List<Project> p2Projects = projects.findAll { project ->
-        project.getProjectCategory()?.getId() ==  10300L && project.getKey() != "SAMPLEP2"   //категория "P2 projects" и исключаем проект "!_Sample_Platform_2 Project"
+        project.getProjectCategory()?.getId() ==  10300L && project.name != "!_Sample_Platform_2 Project"   //категория "P2 projects" и исключаем проект "!_Sample_Platform_2 Project"
     }
     for (Project project : p2Projects) {
         createLinkedEpic(issue, project.getKey())
@@ -99,3 +102,5 @@ if (p2ProjectPickerValue == null) {
         createLinkedEpic(issue, projectInFieldp2ProjectPicker.getKey())
     }
 }
+
+
